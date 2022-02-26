36ec9737c2fc5a4d424ca99cd8df841c41d6eefc
Ratan Kaliani <ratankaliani@berkeley.edu>
2022-02-26 00:28:30 -0800

    temp changes

--- pages/api/proposal.js
+++ pages/api/proposal.js
@@ -20,7 +20,6 @@ export default async function handler(req, res) {
             blockNumber = await getBlockNumber();
         }
         
-        
         let checkPlatforms = false;
         let platformsSet = new Set();
         if ('platforms' in req.query) {
